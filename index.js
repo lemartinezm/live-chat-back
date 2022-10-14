405ecfcb350f116d29ceedb23d480dab067424c6
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-10-13 20:03:24 -0500

    feat: initial config for socket io

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send({
+    status: 200,
+    message: "Hello world",
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (message) => {
+    io.emit("chat message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+});
